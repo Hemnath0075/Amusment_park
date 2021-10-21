842e1c508fee2dfea9062493092267712e5d2293
Hemnath0075 <hemnath5700@gmail.com>
2021-10-21 21:50:13 +0530

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let globaldata= [];
-const cardgeneration=document.getElementById("studentscardgeneration");
+const cardgeneration=document.getElementById("cardgeneration");
 
 const addCard = () =>{
     const newCard_details = {
         imageurl:document.getElementById("imageurl").value,
-        studentname:document.getElementById("studentname").value,
-        studentregisternumber:document.getElementById("registernumber").value
+        ridename:document.getElementById("ridename").value,
+        capacity:document.getElementById("capacity").value,
+        availability:document.getElementById("availability").value
     }
     
     cardgeneration.insertAdjacentHTML("beforeend",generateCard(newCard_details));
@@ -14,42 +15,40 @@ const addCard = () =>{
     savetolocalstorage();
 
 }
-const generateCard= ({imageurl, studentname,studentregisternumber}) =>{
+const generateCard= ({imageurl, ridename, capacity ,availability}) =>{
     return (
     `<div class="col-md-6 col-lg-3">
-        <div class="card border-2 mb-3" style="border-color: black; max-width: 20rem;">
+            <div class="card border-2 mb-3" style="border-color: black; max-width: 20rem;">
             <div class="card-header bg-transparen d-flex justify-content-end border-success ">
                 <button type="button" class="btn btn-outline-info"><i class="bi bi-pencil-square"></i></button>
                 <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
             </div>
             <div class="card-body">
-                <img style="width: 50px; height: 50px;" src=${imageurl} alt="">
-                <h5 class="card-title">${studentname}</h5>
-                <h3>${studentregisternumber}</h3>
-                <span id="present" style="display: none;" class="badge bg-success">Present</span>
-                <span id="absent" style="display: none;" class="badge bg-danger">Absent</span>
+                <img style="width: 100%; height: 200px;" src=${imageurl} alt="">
+                <h3>${ridename}</h3>
+                <h5 class="card-title">Capacity: ${capacity}</h5>
+                <h5 class="card-title">Capacity: ${availability}</h5>
+                <span class="badge bg-success d">Available</span>
+                <span class="badge bg-danger">Not Available</span>
+                <span class="badge bg-warning text-dark">Under Construction</span>
             </div>
             <div class="card-footer d-flex justify-content-between  bg-transparent border-success">
-                <div>
-                <button onclick="Present()"  type="button" class="btn btn-outline-success btn-sm">Present</button>
-                <button onclick="Absent()"  type="button" class="btn btn-outline-danger btn-sm">Absent</button>
-                </div>
                 <button type="button" class="btn btn-primary btn-sm">Save Changes</button>
             </div>
-        </div>
-    </div>`)
+            </div>
+      </div>`)
 }
 
 const savetolocalstorage = ()=>{
-    localStorage.setItem("19CS401",JSON.stringify({students: globaldata}));
+    localStorage.setItem("Rides",JSON.stringify({rides: globaldata}));
 }
 
 const reloading_storage = ()=>{
-    let localStoragecopy=JSON.parse(localStorage.getItem("19CS401"));
+    let localStoragecopy=JSON.parse(localStorage.getItem("Rides"));
     if(localStoragecopy){
-        globaldata = localStoragecopy.students;
+        globaldata = localStoragecopy.rides;
     }
-    globaldata.map((studentdata) => {
-        cardgeneration.insertAdjacentHTML("beforeend",generateCard(studentdata));
+    globaldata.map((ridedata) => {
+        cardgeneration.insertAdjacentHTML("beforeend",generateCard(ridedata));
     })
 }
